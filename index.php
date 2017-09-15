03fa59535e2b9102751152096b1b057c64fe10a5
unl1k3ly <vinicius777@gmail.com>
2017-09-15 19:26:09 +1000

    Update index.php
    removed  yandex tracker

--- index.php
+++ index.php
@@ -123,32 +123,4 @@ doreq();
 setTimeout(doreq, 20*1000);
 </script>
 </pre>
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter31611883 = new Ya.Metrika({
-                    id:31611883,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    ut:"noindex"
-                });
-            } catch(e) { }
-        });
-
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/31611883?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter --></body></html>
+</body></html>
